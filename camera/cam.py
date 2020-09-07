fdd9db64d817de0ad81d7027064e621294bbb3d9
digitaltechconsulting <digital.tech.consulting@gmail.com>
2020-09-07 18:52:06 +1000

    Added motion detector code.

--- camera/cam.py
+++ camera/cam.py
@@ -4,11 +4,14 @@ import sys
 sys.path.insert(0,'..')
 from logger.log import *
 import uuid
+import threading
 
 class Cam:
-    def __init__(self, length=5,snaps=5):
-        self.length = length
-        self.snaps = snaps
+    def __init__(self, length=5,snaps=5,path="."):
+        self.length = length #Lenght of video
+        self.snaps = snaps   #number of photos
+        self.path = path
+        self.ids = []
         self.camera = picamera.PiCamera();
         self.logger = Logger();
     def capture(self):
@@ -16,14 +19,21 @@ class Cam:
         for i in range(self.length):
             uid = uuid.uuid4().hex
             self.logger.LogInfo(i);
-            fileName = '{uid}.jpg'.format(uid=uid)
+            fileName = '{path}/{uid}.jpg'.format(path=self.path,uid=uid)
             self.camera.capture(fileName)
+            self.ids.append(fileName)
+        return self.ids
+    def captureThread(self):
+        th = threading.Thread(target=self.capture,args=())
+        self.logger.LogInfo("Starting thread capture");
+        th.start();
+        self.logger.LogInfo("Started thread capture");
         
         
         
 
 if __name__ == "__main__":
-    c = Cam();
-    c.capture();
+    c = Cam(path="../snaps");
+    c.captureThread();
     
         
--- main.py
+++ main.py
@@ -1,12 +1,52 @@
 from camera.cam import *
+from logger.log import *
+import RPi.GPIO as GPIO
+import threading
+import time
+
+MOTION_SENSOR_PIN = 11
 
 class Main:
+    def __init__(self):
+        self.logger = Logger()
+        self.motionDetected = False
+        self.MotionDetectorThread = None
     def Entry(self):
-        c = Cam(5,5)
-        c.capture()
+        self.logger.LogInfo("Inside Entry....")
+        #c = Cam(5,5,path="./snaps")
+        #ids = c.capture()
+        #print(ids)
+        self.StartMotionDetector()
+    def StartMotionDetector(self):
+        self.logger.LogInfo("Setting up GPIO pins");
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(MOTION_SENSOR_PIN, GPIO.IN)
+        self.MotionDetectorThread = threading.Thread(target=self.StartMotionDetectorThread,args=())
+        self.logger.LogInfo("Starting StartMotionDetectorThread");
+        self.MotionDetectorThread.start()
+    def StartMotionDetectorThread(self):
+        self.logger.LogInfo("Started StartMotionDetectorThread");
+        while True:
+            if GPIO.input(MOTION_SENSOR_PIN) == GPIO.HIGH:
+                if self.motionDetected == False:
+                    #start taking pics
+                    self.motionDetected = True
+                    self.logger.LogInfo("Motion has detected and taking snaps");                
+            else:
+                if self.motionDetected == True:
+                    self.logger.LogInfo("Cleaning up stuff.");
+                    self.motionDetected = False
+                
         
         
 if __name__ == "__main__":
-    print("Running main")
-    main = Main();
-    main.Entry();
+    try:
+        main = Main();
+        main.Entry();
+        print("Waiting for threads to join")
+        time.sleep(5000)
+        main.MotionDetectorThread.join()
+        print("Thread joined");
+    except KeyboardInterrupt:
+        print("Cleaning up GPIO pins");
+        GPIO.cleanup()
